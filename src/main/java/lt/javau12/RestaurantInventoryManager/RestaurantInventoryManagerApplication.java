398bb9a8c6bbffda7413dd9d88ccb2a4fa996ada
Gvidas Kalvonis <gvidaskalv@gmail.com>
2025-05-19 09:22:17 +0300

    Initial project commit

---
+++ src/main/java/lt/javau12/RestaurantInventoryManager/RestaurantInventoryManagerApplication.java
@@ -0,0 +1,13 @@
+package lt.javau12.RestaurantInventoryManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantInventoryManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantInventoryManagerApplication.class, args);
+	}
+
+}
