e1dffe627d1d2204d642fa40a0b4a3d18ca18201
Gvidas Kalvonis <gvidaskalv@gmail.com>
2025-05-20 09:13:22 +0300

    Unit CRUD operations added

---
+++ src/main/java/lt/javau12/RestaurantInventoryManager/controllers/UnitController.java
@@ -0,0 +1,50 @@
+package lt.javau12.RestaurantInventoryManager.controllers;
+
+import lt.javau12.RestaurantInventoryManager.dtos.UnitDTO;
+import lt.javau12.RestaurantInventoryManager.services.UnitService;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/inventory/units") //EXAMPLE URL localhost:8080/api/inventory/units
+public class UnitController {
+
+    private final UnitService unitService;
+
+    public UnitController(UnitService unitService){
+        this.unitService = unitService;
+    }
+
+
+    @GetMapping("/all") //EXAMPLE URL localhost:8080/api/inventory/units/all
+    public ResponseEntity<List<UnitDTO>> getAllUnits(){
+        return ResponseEntity.ok(unitService.getAllUnits());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UnitDTO> getUnitById(@PathVariable Long id){
+        return ResponseEntity.ok(unitService.getUnitById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<UnitDTO> create(@RequestBody UnitDTO unitDTO){
+        UnitDTO unitCreated = unitService.create(unitDTO);
+        return ResponseEntity.status(HttpStatusCode.valueOf(201)).body(unitCreated);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UnitDTO> update(@RequestBody UnitDTO unitDTO, @PathVariable Long id){
+        return ResponseEntity.ok(unitService.update(unitDTO, id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        return unitService.delete(id)
+                ? ResponseEntity.noContent().build()
+                : ResponseEntity.notFound().build();
+    }
+
+}
---
+++ src/main/java/lt/javau12/RestaurantInventoryManager/services/UnitService.java
@@ -0,0 +1,53 @@
+package lt.javau12.RestaurantInventoryManager.services;
+
+import lt.javau12.RestaurantInventoryManager.dtos.UnitDTO;
+import lt.javau12.RestaurantInventoryManager.entities.Unit;
+import lt.javau12.RestaurantInventoryManager.mappers.UnitMapper;
+import lt.javau12.RestaurantInventoryManager.repositories.UnitRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UnitService {
+
+    private final UnitRepository unitRepository;
+    private final UnitMapper unitMapper;
+
+    public UnitService(UnitRepository unitRepository, UnitMapper unitMapper){
+        this.unitRepository = unitRepository;
+        this.unitMapper = unitMapper;
+    }
+
+    public List<UnitDTO> getAllUnits(){
+        List<Unit> units = unitRepository.findAll();
+        return units.stream().map(unitMapper::toDTO).toList();
+    }
+
+    public UnitDTO getUnitById(Long id){
+        Unit unit = unitRepository.findById(id).orElseThrow( () -> new RuntimeException("Unit not found with Id" + id) );
+        return unitMapper.toDTO(unit);
+    }
+
+    public UnitDTO create(UnitDTO unitDTO){
+        Unit unitToBeCreated = unitMapper.toEntity(unitDTO);
+        Unit unitCreated = unitRepository.save(unitToBeCreated);
+        return unitMapper.toDTO(unitCreated);
+    }
+
+    public UnitDTO update(UnitDTO unitDTO, Long id){
+        Unit unit = unitRepository.findById(id).orElseThrow( () -> new RuntimeException("Unit not found with Id" + id) );
+        unit.setName(unitDTO.getName());
+        Unit updatedUnit = unitRepository.save(unit);
+        return unitMapper.toDTO(updatedUnit);
+    }
+
+    public boolean delete(Long id){
+        if( unitRepository.existsById(id) ){
+            unitRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+}
